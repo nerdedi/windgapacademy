a33d26459a3190ffb4b12f1c04c70e993011a616
nerdedi <nerdedi@windgap.org.au>
2025-08-19 16:59:56 +1000

    Jest Tests

---
+++ components/AvatarBuilder.test.js
@@ -0,0 +1,36 @@
+import { getAvatarState } from './AvatarBuilder';
+
+describe('getAvatarState', () => {
+  it('should return correct avatar state with all properties', () => {
+    const features = {
+      skin: '#fff',
+      hair: '#000',
+      outfit: 'casual',
+      pronoun: 'they/them',
+      accessory: 'glasses',
+      wheelchair: true,
+      wheelchairColour: '#1976d2',
+      wheelchairSize: 100
+    };
+    expect(getAvatarState(features)).toEqual(features);
+  });
+
+  it('should handle missing optional properties', () => {
+    const features = {
+      skin: '#fff',
+      hair: '#000',
+      outfit: 'casual',
+      pronoun: 'she/her'
+    };
+    expect(getAvatarState(features)).toEqual({
+      skin: '#fff',
+      hair: '#000',
+      outfit: 'casual',
+      pronoun: 'she/her',
+      accessory: undefined,
+      wheelchair: undefined,
+      wheelchairColour: undefined,
+      wheelchairSize: undefined
+    });
+  });
+});
