a88832153c5a4a62953002ccd159e5a5a2d47f40
nerdedi <nerdedi@windgap.org.au>
2025-08-26 10:07:50 +0000

    error and jest config update

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
-export default {
-  babelConfig: "./babel.config.cjs",
-  testEnvironment: "jsdom",
-  roots: ["<rootDir>/src", "<rootDir>/components"],
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  bail: 1,
 };
+
+module.exports = config;
