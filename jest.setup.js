4bdad915784655b165aaa86d36794218355bce8e
nerdedi <nerdedi@windgap.org.au>
2025-09-02 10:26:21 +0000

    chore: update CI workflow, enhance Jest configuration, and add polyfills

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,15 @@
 require("@testing-library/jest-dom");
+
+// Polyfill TextEncoder for Node.js tests (some deps rely on it)
+if (typeof global.TextEncoder === 'undefined') {
+	global.TextEncoder = require('util').TextEncoder;
+}
+
+// Provide a harmless window.alert implementation for jsdom tests
+if (typeof window !== 'undefined' && typeof window.alert !== 'function') {
+	window.alert = (msg) => {
+		// no-op to keep tests from throwing when components call alert
+		// eslint-disable-next-line no-console
+		console.info('window.alert called during test:', msg);
+	};
+}
