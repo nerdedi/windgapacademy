9c5d19c22bf564f979df10570a64879656ce8ce9
nerdedi <nerdedi@windgap.org.au>
2025-09-03 14:40:26 +0000

    test: remove leftover GamePlayground JSX test to fix Jest parsing

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -7,7 +7,7 @@ import Navbar from "../../components/Navbar.jsx";
 import ErrorBoundary from "../components/ErrorBoundary.jsx";
 import ProtectedRoute from "./ProtectedRoute.jsx";
 const HomePage = lazy(() => import("../pages/HomePage.jsx"));
-const EducatorDashboard = lazy(() => import("../pages/EducatorDashboard.jsx"));
+const EducatorDashboard = lazy(() => import("../pages/EducatorDashboard.tsx"));
 const LearnerDashboard = lazy(() => import("../pages/LearnerDashboard.jsx"));
 const GamePlayground = lazy(() => import("../../components/GameModules/GamePlayground.jsx"));
 const CalmSpaceSimulation = lazy(() => import("../../components/CalmSpaceSimulation.jsx"));
