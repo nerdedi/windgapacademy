87783346f2fa1beac6de401e9a8051cf10215c57
nerdedi <nerdedi@windgap.org.au>
2025-09-03 12:05:10 +0000

    refactor: add ErrorBoundary, lazy-load routes, and update main router for Suspense/code-splitting

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
@@ -9,6 +10,6 @@ if (container) {
   root.render(
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
+    </BrowserRouter>
   );
 }
