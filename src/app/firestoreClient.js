5040ea246dad8fe94801c4d246cdf65977378619
nerdedi <nerdedi@windgap.org.au>
2026-01-13 06:57:00 +0000

    feat: Refactor authentication and Firestore helpers to use ES module syntax

--- src/app/firestoreClient.js
+++ src/app/firestoreClient.js
@@ -1,11 +1,12 @@
-// Centralized Firestore helpers. Uses lazy requires to avoid breaking test envs.
+// Centralized Firestore helpers using ES module syntax.
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
+import { app } from "../../firebase.js";
+
 let _db;
 function getDb() {
   if (_db) return _db;
   try {
-    // eslint-disable-next-line global-require
-    const { getFirestore } = require("firebase/firestore");
-    _db = getFirestore();
+    _db = app ? getFirestore(app) : getFirestore();
     return _db;
   } catch (e) {
     // eslint-disable-next-line no-console
@@ -14,21 +15,17 @@ function getDb() {
   }
 }
 
-async function getUserDoc(uid) {
+export async function getUserDoc(uid) {
   const db = getDb();
   if (!db) return null;
-  // eslint-disable-next-line global-require
-  const { doc, getDoc } = require("firebase/firestore");
   const snap = await getDoc(doc(db, "users", uid));
   return snap.exists() ? snap.data() : null;
 }
 
-async function setUserDoc(uid, data) {
+export async function setUserDoc(uid, data) {
   const db = getDb();
   if (!db) throw new Error("Firestore not available");
-  // eslint-disable-next-line global-require
-  const { doc, setDoc } = require("firebase/firestore");
   await setDoc(doc(db, "users", uid), data, { merge: true });
 }
 
-module.exports = { getDb, getUserDoc, setUserDoc };
+export { getDb };
