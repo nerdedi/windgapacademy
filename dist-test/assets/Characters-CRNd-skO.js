13ece2d5d6a504f92a334effac56da0a1828d59e
nerdedi <nerdedi@windgap.org.au>
2026-01-14 07:03:17 +0000

    Add service worker and test server page for PWA functionality
    - Implemented a basic service worker for caching and offline support.
    - Created a test server HTML page to verify server status and JavaScript functionality.

---
+++ dist-test/assets/Characters-CRNd-skO.js
@@ -0,0 +1,2 @@
+function r(a){return{language:{name:"Lana",avatar:"🦉"},literacy:{name:"Leo",avatar:"🦁"},numeracy:{name:"Nia",avatar:"🐧"},digital:{name:"Dex",avatar:"🦊"},independence:{name:"Indy",avatar:"🐨"}}[a]||{name:"Guide",avatar:"👤"}}export{r as g};
+//# sourceMappingURL=Characters-CRNd-skO.js.map
